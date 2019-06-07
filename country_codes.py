e614e03aca04bc6a3f783f4305885212d9043b19
wyj226 <308033240@qq.com>
2019-06-07 15:59:50 +0800

    final

---
+++ country_codes.py
@@ -0,0 +1,10 @@
+#i18n为国别码模块，字典COUNTRIES包含的键值对为两个字母的国别码和国家名
+from pygal_maps_world.i18n import COUNTRIES
+
+def get_country_code(country_name):
+    """根据指定的国家，返回Pygal使用的两个字母的国别码"""
+    for code, name in COUNTRIES.items():
+        if name == country_name:
+            return code
+        #如果没有找到指定国家，就返回None
+    return None
